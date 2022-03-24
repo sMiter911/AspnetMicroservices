6f6ca700756e84919c781e0620bd4d283dcaa2dd
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-03-24 21:13:59 +0200

    Setup CRUD with Mongo Container

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,19 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+using Catalog.API.Repositories.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var services = builder.Services;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Configure DI for application services
+services.AddScoped<ICatalogContext, CatalogContext>();
+services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
